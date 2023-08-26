1041d3d567face336890f35fdd45dab5344f9b53
riyazu-ddin <ddinriyazu81@gmail.com>
2023-08-26 22:34:06 +0530

    added initial files

---
+++ javarun.js
@@ -0,0 +1,41 @@
+// let x = 5;
+// if (x > 3) {
+//   let x = 10;
+//   console.log(x);
+// }
+// console.log(x);
+
+
+// .logo {
+//     color: #fff;
+//     margin-left: 40px;
+//   }
+  nav {
+    color: #fff;
+    display: flex;
+    width: 100%;
+    padding: 15px;
+    align-items: center;
+    justify-content: space-between;
+  }
+  nav ul li {
+    color: #fff;
+    display: inline-block;
+    list-style: none;
+    margin: 10px 30px;
+  }
+  nav ul li a {
+    text-decoration: none;
+    list-style: none;
+  }
+//   nav ul li a:hover {
+//     color: blueviolet;
+//   }
+//   .image {
+//     margin: 60px;
+//     width: 45%;
+//     height: 80%;
+//     position: absolute;
+//     right: 100px;
+//   }
+  
